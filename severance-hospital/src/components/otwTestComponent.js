c8dfc6fd183b448eb08d5422dbd68263b40448fb
otw03 <86701873+otw03@users.noreply.github.com>
2022-11-15 16:15:09 +0900

    otw test component merge

---
+++ severance-hospital/src/components/otwTestComponent.js
@@ -0,0 +1,13 @@
+import React, {memo, useCallback} from 'react';
+import styled from 'styled-components';
+import { Link, Routes, Route } from "react-router-dom";
+
+const otwTestComponent = memo(() => {
+    return (
+        <div>
+            <h1>Team-3</h1>
+            <h2>otw Test Component</h2>
+        </div>
+    );
+});
+export default otwTestComponent;
