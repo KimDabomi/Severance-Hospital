e93a8192251b868bb6873d65accc754059d08baa
davidpark-0098 <davidpark.0098@gmail.com>
2022-11-16 16:57:07 +0900

    meta, package add

---
+++ severance-hospital/src/Meta.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+const Meta = (props) => {
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{props.title}</title>
+
+        <meta name="description" content={props.description} />
+        <meta name="keywords" content={props.keywords} />
+        <meta name="author" content={props.author} />
+        <meta name="subject" content={props.subject} />
+        <meta name="copyright" content={props.copyright} />
+        <meta name="content-language" content="ko" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:description" content={props.description} />
+        <meta property="og:url" content={props.url} />
+        <meta property="og:image" content={props.image} />
+        <link rel="icon" href={props.icon} type="image/png" />
+        <link rel="shortcut icon" href={props.chortcutIcon} type="image/png" />
+        <link rel="apple-touch-icon" href={props.appleTouchIcon} type="image/png" />
+      </Helmet>
+    </HelmetProvider>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Severance Hospital",
+  description: "Severance Hospital",
+  author: "EZEN Academy Team 3",
+  subject: "Severance Hospital Clone Project",
+  copyright: "EZEN Academy Team 3",
+  keywords: "Severance Hospital Clone Project",
+  url: window.location.href,
+  image: null,
+  icon: null,
+  shortcutIcon: null,
+  appleTouchIcon: null
+};
+
+export default Meta;
