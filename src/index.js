d1f664edc055ba4974f7868c082fc53dc14a4297
davidpark-0098 <davidpark.0098@gmail.com>
2022-11-23 18:15:55 +0900

    comment: store 부분 주석처리

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import App from './Test';
 import { BrowserRouter } from "react-router-dom";
 
 /** redux 구성을 위한 참조 */
-import { Provider } from "react-redux"; // provider을 통해 store 구독
-import store from "./store"; // 전체 slice를 포함한 store
+// import { Provider } from "react-redux"; // provider을 통해 store 구독
+// import store from "./store"; // 전체 slice를 포함한 store
 
 /** Meta, GlobalStyles */
 import Meta from "./Meta";
@@ -22,11 +22,11 @@ import GlobalStyles from "./GlobalStyles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // store 속성 명시
-  <Provider store={store}>
+  // <Provider store={store}>
     <BrowserRouter>
       <Meta />
       <GlobalStyles />
       <App />
     </BrowserRouter>
-  </Provider>
+  // </Provider>
 );
