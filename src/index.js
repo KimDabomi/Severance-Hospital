c59305f0eade8d708d61b20769d31123bd7eb684
Joohyeji <rosyjoo1999@gmail.com>
2022-11-25 17:15:38 +0900

    customer오류 fix

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ import App from './Test';
 import { BrowserRouter } from "react-router-dom";
 
 /** redux 구성을 위한 참조 */
-// import { Provider } from "react-redux"; // provider을 통해 store 구독
-// import store from "./store"; // 전체 slice를 포함한 store
+import { Provider } from "react-redux"; 
+// provider을 통해 store 구독
+import store from "./store"; 
+// 전체 slice를 포함한 store
 
 /** Meta, GlobalStyles */
 import Meta from "./Meta";
