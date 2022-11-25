b315b4e88783d04546715f8eb405a34e06f6db87
davidpark-0098 <davidpark.0098@gmail.com>
2022-11-25 15:34:44 +0900

    del

--- README.md
+++
@@ -1,3 +0,0 @@
-# Severance-Hospital
-
-Severance Hospital
--- src/components/MyPage.js
+++
@@ -1,9 +0,0 @@
-import React,{memo} from 'react';
-
-const Mypage = memo(() => {
-  return (
-    <div>Mypage</div>
-  );
-});
-
-export default Mypage;
