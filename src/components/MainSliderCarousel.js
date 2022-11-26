07bee41ea7c2515cbc0dc64c2b3e1959de8ca0c4
davidpark-0098 <davidpark.0098@gmail.com>
2022-11-27 02:17:58 +0900

    design: 추가 메인페이지 슬라이드 메인페이지의 메인슬라이드 slick을 사용한 슬라이드 구현

---
+++ src/components/MainSliderCarousel.js
@@ -0,0 +1,113 @@
+/**
+ * @ File Name: MainSliderCarousel.js
+ * @ Author: 박다윗 (davidpark.0098@gmail.com)
+ * @ Last Update: 2022-11-27 15:02:00
+ * @ Description: Slick 슬라이드
+ */
+
+/** import */
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+// slick
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+/** 이미지 참조 */
+// 메인 슬라이드
+import MainSlideImageFirst from "../assets/img/img-visual-patient1.jpg";
+import MainSlideImageSecond from "../assets/img/img-home-visual_221117.png";
+
+/** 슬라이드 전체 박스 스타일 */
+const SlideContainer = styled.div`
+`;
+
+// 슬라이드 영역 스타일
+const StyledSlider = styled(Slider)`
+  width: 1920px;
+  height: 500px;
+  position: relative;
+
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+
+  .slick-slide div {
+    //슬라이더  컨텐츠
+    cursor: pointer;
+  }
+`;
+
+/** 이미지 슬라이드 스타일 */
+const SlideSection = styled.section`
+  width: 1920px;
+  position: relative;
+
+  img {
+    display: block;
+    max-width: 1920px;
+    height: auto;
+    margin: 0 auto;
+  }
+
+  article {
+    position: absolute;
+    top: 50%;
+    left: 320px;
+    transform: translate(0, -50%);
+
+    .slide_title {
+      font-size: 66px;
+      line-height: 72px;
+      font-family: "NanumSquare";
+      color: white;
+    }
+
+    .slide_text {
+      font-size: 24px;
+      line-height: 32px;
+      font-family: "NanumSquare";
+      color: white;
+      display: block;
+      margin-top: 15px;
+    }
+  }
+`;
+
+function InfoSliderCarousel() {
+  // 슬라이드 설정
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    slideToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true
+  };
+
+  return (
+    <SlideContainer>
+      <StyledSlider {...settings}>
+        <SlideSection>
+          <img src={MainSlideImageFirst} alt="main_img" />
+          <article>
+            <span className="slide_title">
+              <strong>공감,</strong> 또 하나의 치료
+            </span>
+            <span className="slide_text">질병 치료를 넘어 환자의 마음까지 치유하겠습니다.</span>
+          </article>
+        </SlideSection>
+        <SlideSection>
+          <img src={MainSlideImageSecond} alt="main_img" />
+        </SlideSection>
+      </StyledSlider>
+    </SlideContainer>
+  );
+}
+
+export default InfoSliderCarousel;
