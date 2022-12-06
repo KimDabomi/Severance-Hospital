684f297773d416d54e4730c64498ec9ff822eccc
Joohyeji <rosyjoo1999@gmail.com>
2022-12-06 19:17:14 +0900

    22.12.06 newshome youtube css add

--- src/pages/NewsHome/NewsMain.js
+++ src/pages/NewsHome/NewsMain.js
@@ -1,20 +1,36 @@
 /**
  * @ File Name: NewsMain.js
  * @ Author: 주혜지 (rosyjoo1999@gmail.com)
- * @ Last Update: 2022-12-03
+ * @ Last Update: 2022-12-06
  * @ Description: 뉴스 메인 페이지
  */
 
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/MainPageHeader';
 import Footer from '../../components/Footer';
 import corona from '../../assets/img/corona.jpg';
+import { useSelector, useDispatch } from 'react-redux';
+import { getYoutube } from '../../slices/YoutubeSlice';
 
 import NewsHomeCarousel from '../../components/NewsHomeCarousel';
 import NewsHomeNoticeCarousel from '../../components/NewsHomeNoticeCarousel';
 
 const NewsMain = memo(() => {
+  //dispatch함수 생성
+	const dispatch = useDispatch();
+	//hook을 통해 slice가 관리하는 상태값 가져오기
+	const { data, error } = useSelector((state) => state.YoutubeSlice);
+
+  /** 최초마운트시 리덕스를 통해 목록을 조회한다. */
+  useEffect(()=>{
+    dispatch(getYoutube());
+  if(data){
+    console.log(data);
+  }
+  },[])
+
+
   return (
     <div>
       <Header />
@@ -62,11 +78,29 @@ const NewsMain = memo(() => {
         <div className="tabContentCont pageCont">
           <div className="tabContent" id="tabContentFacebook">
             <h3 className="title3">페이스북</h3>
-            <div className="contentBox">{/* 동영상 및 글 */}content</div>
+            <div className="contentBox">content</div>
           </div>
           <div className="tabContent" id="tabContentFacebook">
             <h3 className="title3">유튜브</h3>
-            <div className="contentBox">{/* 동영상 및 글 */}content</div>
+            <div className="contentBox">
+              <div className="youtubeArticle">
+                {data && 
+                  data.map((v,i)=>{
+                    return(
+                      <div className="thumbItem" key={i}>
+                        <Link to='https://www.youtube.com/watch?v=DJTWAXxj0fQ' target="_blank">
+                          <div className="thumb">
+                            <img src={v.snippet.thumbnails.medium.url} alt='새창'></img>
+                            <i className='icoMoviePlay'></i>
+                          </div>
+                          <div className="subjectArea">{v.snippet.title}<strong></strong></div>
+                        </Link>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
           </div>
         </div>
       </div>
