94d5328520e2d95f279aef59f84650d69fd1d9e6
davidpark-0098 <davidpark.0098@gmail.com>
2022-12-21 21:23:24 +0900

    feat: 추가 이용약관, 개인정보처리방침 페이지

---
+++ src/pages/Policy/Privacy.js
@@ -0,0 +1,21 @@
+/**
+ * @ File Name: Privacy.js
+ * @ Author: 박다윗 (davidpark.0098@gmail.com)
+ * @ Last Update: 2022-12-21 15:02:00
+ * @ Description: 개인정보처리방침 페이지
+ */
+
+/** import */
+import React, { memo } from "react";
+import styled from "styled-components";
+
+const Hospital = memo(() => {
+  return (
+    <>
+      {/* 페이지 타이틀 */}
+      <h1 className="pageTitle">개인정보처리방침</h1>
+    </>
+  );
+});
+
+export default Hospital;
