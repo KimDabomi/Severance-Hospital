3bc5b9af3b10a3784e97addc93108b699d7b98ae
davidpark-0098 <davidpark.0098@gmail.com>
2022-12-23 18:04:41 +0900

    feat: 추가 회원 예약 컴포넌트

--- src/pages/Appointment/Reserve1.js
+++ src/pages/Appointment/Reserve1.js
@@ -14,6 +14,84 @@ import styled from "styled-components";
 // 회원 예약 이미지
 import bgBox02 from "../../assets/img/bg-box-02.png";
 import box02 from "../../assets/img/ico-box-02.png";
+import radiocheck from "../../assets/img/ico-radio-checked.png";
+
+/** 임시 cont */
+const Cnt = styled.div`
+  display: flex;
+`;
+/** 회원 예약 박스 스타일 */
+const ReserveLogin = styled.div`
+  width: 305px;
+  height: 803px;
+
+  position: relative;
+  margin: 0 10px;
+
+  font-size: 14px;
+  text-align: center;
+
+  display: block;
+  border: 1px solid #e6e6e6;
+  box-sizing: border-box;
+
+  /* &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    border: 1px solid #e6e6e6;
+  } */
+
+  .title {
+    font-size: 20px;
+    font-weight: 700;
+    padding: 24px 0;
+  }
+
+  .username {
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 285px;
+    background-color: #f9f9f9;
+  }
+
+  .btns {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 40px;
+    margin: 0 20px;
+    box-sizing: border-box;
+
+    button {
+      width: 100%;
+      height: 50px;
+      margin-top: 20px;
+
+      font-size: 18px;
+
+      border: 2px solid transparent;
+      border-color: #959595;
+      border-radius: 25px;
+      background-color: #fff;
+      box-sizing: border-box;
+
+      color: #0094fb;
+      border-color: #0094fb;
+      background-color: #fff;
+
+      &:first-child {
+        color: #333;
+        border-color: #959595;
+        background-color: #fff;
+      }
+    }
+  }
+`;
 
 /** 회원 예약 박스 스타일 */
 const Member = styled.div`
@@ -67,6 +145,123 @@ const Member = styled.div`
 
         text-align: center;
         line-height: 1.25;
+      }
+    }
+  }
+`;
+
+/** 회원 예약 양식 */
+const ReserveBox = styled.div`
+  width: 305px;
+  height: 803px;
+
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+
+  background: #fff;
+  border: 1px solid #e6e6e6;
+  box-sizing: border-box;
+
+  display: none;
+  font-size: 14px;
+  text-align: center;
+
+  .title {
+    font-size: 20px;
+    font-weight: 700;
+    padding: 24px 0;
+  }
+
+  .form {
+    text-align: left;
+    .radios {
+      background-color: #f9f9f9;
+      padding: 17px 20px 20px;
+      margin-bottom: 14px;
+      box-sizing: border-box;
+
+      span {
+        line-height: 20px;
+        vertical-align: middle;
+      }
+    }
+
+    .agree {
+      margin: 0 20px;
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    .boxsCF {
+      background-color: #f9f9f9;
+      padding: 15px 20px;
+
+      p:first-child {
+        font-weight: bold;
+        padding-bottom: 15px;
+      }
+      p:nth-child(2) {
+        font-size: 14px;
+      }
+
+      //동의체크박스
+      .agreeSelect {
+        padding-top: 40px;
+
+        span:last-child {
+          padding-left: 15px;
+        }
+
+        label {
+          font-size: 13px;
+        }
+
+        [type="radio"] {
+          vertical-align: middle;
+          appearance: none;
+          border: 1px solid gray;
+          background-color: white;
+          border-radius: 50%;
+          width: 1.25em;
+          height: 1.25em;
+          margin: -3px 5px 0 0;
+        }
+
+        [type="radio"]:checked {
+          border: 1px solid black;
+          background: #fff url(${radiocheck}) no-repeat center center;
+          background-size: 11px 11px;
+        }
+      }
+    }
+  }
+
+  .btns {
+    position: absolute;
+    bottom: 40px;
+    left: 0;
+    right: 0;
+    margin: 0 20px;
+
+    button {
+      width: 100px;
+      height: 50px;
+      padding: 0 28px;
+      font-size: 18px;
+
+      color: #333;
+      background-color: #fff;
+      border: 2px solid #959595;
+      border-radius: 25px;
+
+      &:last-child {
+        margin-left: 6px;
+        color: #fff;
+        background-color: #0094fb;
+        border: 2px solid transparent;
       }
     }
   }
@@ -136,7 +331,6 @@ const Reserve1 = memo(() => {
   const location = useLocation();
 
   /** ref */
-  const loginPopup = useRef();
   const joinPopup = useRef();
   const login = useRef();
   const member = useRef();
@@ -149,56 +343,109 @@ const Reserve1 = memo(() => {
   /** 팝업 */
   // 팝업 열기
   const openPopup = useCallback((e) => {
-    e.target.dataset.intro === "loginPopup" ? (loginPopup.current.style.visibility = "visible") : (joinPopup.current.style.visibility = "visible");
+    joinPopup.current.style.visibility = "visible";
   }, []);
 
   // 팝업 닫기
   const closePopup = useCallback((e) => {
-    loginPopup.current.style.visibility = "hidden";
     joinPopup.current.style.visibility = "hidden";
   }, []);
 
   // URL 경로 이동 시, 팝업을 닫는다.
   useEffect(() => {
-    loginPopup.current.style.visibility = "hidden";
     joinPopup.current.style.visibility = "hidden";
   }, [location.pathname]);
 
   return (
-    <>
+    <Cnt>
+      {/* 회원 정보 */}
+      <ReserveLogin>
+        <p className="title">회원 정보</p>
+        <p className="username">
+          <span>박다윗</span> 님
+        </p>
+        <div className="btns">
+          <button type="button">로그아웃</button>
+          <button type="button">MY세브란스</button>
+          <button type="button">예약 현황 조회</button>
+          <button type="button">과거 진료이력 확인</button>
+        </div>
+      </ReserveLogin>
+
       {/* 회원 예약 */}
       <Member ref={member}>
-        <a href="#login" data-intro="member" onClick={switchDisplay}>
-          <div className="iconCont">
+        <a href="#reserve" data-intro="member" onClick={switchDisplay}>
+          <div className="iconCont">``
             <i />
             <span>회원 예약</span>
           </div>
         </a>
 
         {/* 회원 예약 로그인, 회원가입 */}
-        <Login ref={login}>
-          <p>회원 예약</p>
-          <div className="btns">
-            <button type="button" data-intro="loginPopup" onClick={openPopup}>
-              로그인
-            </button>
-            <button type="button" data-intro="joinPopup" onClick={openPopup}>
-              회원가입
-            </button>
+        <ReserveBox ref={login}>
+          <p className="title">회원 예약</p>
+          <div className="form">
+            <div className="radios">
+              <span>
+                <input type="radio" name="radio" id="radio1" />
+                <label htmlFor="radio1">본인 예약</label>
+              </span>
+
+              <span>
+                <input type="radio" name="radio" id="radio2" />
+                <label htmlFor="radio2">대리 예약</label>
+              </span>
+            </div>
+
+            <p className="agree">1. 본인 정보 입력</p>
+
+            <dl>
+              <dt>
+                <label>성명</label>
+              </dt>
+              <dd>
+                <input type="text" name="certForm_rsvctmNm" id="u_name" data-error-message="성명을 입력해주세요." defaultValue="박다윗" />
+              </dd>
+
+              <dt>
+                <label>연락처</label>
+              </dt>
+              <dd></dd>
+            </dl>
+
+            <div className="boxsCF">
+              <p>개인정보 수집·이용 동의</p>
+              <p>개인정보는 병원정보 조회를 위해서만 사용합니다. 개인정보 이용에 동의합니다.</p>
+
+              <form className="agreeSelect">
+                <span className="radioItem">
+                  <label>
+                    <input type="radio" id="mdAgree1" name="agree" />
+                    동의합니다.
+                  </label>
+                </span>
+                <span className="radioItem">
+                  <label>
+                    <input type="radio" id="mdAgree2" name="agree" />
+                    동의하지 않습니다.
+                  </label>
+                </span>
+              </form>
+            </div>
           </div>
 
-          <ul>
-            <li>회원예약은 세브란스병원 로그인 후 이용하실 수 있습니다.</li>
-            <li>회원이 아닌 분은 회원가입 후 이용 또는 비회원 예약을 이용하시기 바랍니다.</li>
-          </ul>
-
-          <button type="button" className="backBtn" onClick={switchDisplay}>
-            이전
-          </button>
-        </Login>
+          <div className="btns">
+            <button type="button" data-intro="loginPopup" onClick={switchDisplay}>
+              이전
+            </button>
+            <button type="button" data-intro="joinPopup" onClick={openPopup}>
+              다음
+            </button>
+          </div>
+        </ReserveBox>
       </Member>
 
-      {/* 로그인 팝업 */}
+      {/* 로그인 팝업
       <Popup ref={loginPopup}>
         <div className="popupCont">
           <p>로그인 페이지로 이동하시겠습니까?</p>
@@ -211,23 +458,18 @@ const Reserve1 = memo(() => {
             </button>
           </footer>
         </div>
-      </Popup>
+      </Popup> */}
 
       {/* 회원가입 팝업 */}
       <Popup ref={joinPopup}>
         <div className="popupCont">
-          <p>세브란스 회원가입 페이지로 이동하시겠습니까?</p>
+          <p>개인정보 수집·이용 조회 제공에 동의해야 예약이 가능합니다.</p>
           <footer>
-            <Link to="/join_way">
-              <button type="button">확인</button>
-            </Link>
-            <button type="button" className="cancelBtn" onClick={closePopup}>
-              취소
-            </button>
+            <button type="button" onClick={closePopup}>닫기</button>
           </footer>
         </div>
       </Popup>
-    </>
+    </Cnt>
   );
 });
 
