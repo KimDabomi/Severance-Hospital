786a39a9cb565b9a9486a12b13e9abe971c57b0a
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-04 19:41:54 +0900

    feat: 추가 페이지 스크롤 이동 기능 및 주석

--- src/index.js
+++ src/index.js
@@ -2,29 +2,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/** test */
+// App, test
+/**/
 import App from "./App";
-// import App from './test';
+/*/
+import App from './Test';
+/**/
 
-
-/** 라우터 */
+// 라우터
 import { BrowserRouter } from "react-router-dom";
 
-/** redux 구성을 위한 참조 */
-import { Provider } from "react-redux"; 
+/** redux 구성 */
 // provider을 통해 store 구독
-import store from "./store"; 
+import { Provider } from "react-redux";
 // 전체 slice를 포함한 store
+import store from "./store";
 
 /** Meta, GlobalStyles */
 import Meta from "./Meta";
 import GlobalStyles from "./GlobalStyles";
+
+/** 페이지 이동 및 새로고침 시 스크롤 최상단 이동 */
+import ScrollToTop from "./components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // store 속성 명시
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollToTop />
       <Meta />
       <GlobalStyles />
       <App />
