ee2a30aaab1b392a5cd52a0d43f75ba6771e2b7f
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-05 00:24:07 +0900

    feat: 추가 서브라우팅

--- src/pages/Manager/Manager.js
+++ src/pages/Manager/Manager.js
@@ -7,11 +7,17 @@
 
 /** import */
 import React, { memo } from "react";
-import { NavLink, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
+// 서브 라우팅 페이지
 import ManagerCustomerBoard from "./ManagerCustomerBoard";
+import ManagerNews from "./ManagerNews";
+import ManagerNotice from "./ManagerNotice";
+import ManagerCooperationHospital from "./ManagerCooperationHospital";
+import ManagerCooperationDoctor from "./ManagerCooperationDoctor";
 
+/** 전체 메뉴 스타일 */
 const MenuContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -19,7 +25,7 @@ const MenuContainer = styled.div`
   padding: 50px 100px 20px;
   border-bottom: 1px solid #eee;
 `;
-// 메뉴 스타일
+// 각 메뉴 스타일
 const MenuLinkContainer = styled(NavLink)`
   font-size: 18px;
   padding-bottom: 2px;
@@ -35,83 +41,36 @@ const MenuLinkContainer = styled(NavLink)`
     font-weight: bold;
   }
 `;
-const Table = styled.table`
-  border-collapse: collapse;
-  border-top: 3px solid #168;
-  font-size: 14px;
-  text-align: center;
-  margin: auto;
-  width: 100%;
 
-  th {
-    color: #168;
-    background: #f0f6f9;
-    border: 1px solid #ddd;
-
-    &:first-child {
-      border-left: 0;
-    }
-
-    &:last-child {
-      border-right: 0;
-    }
-  }
-
-  td {
-    padding: 10px;
-    border: 1px solid #ddd;
-
-    &:first-child {
-      border-left: 0;
-    }
-
-    &:last-child {
-      border-right: 0;
-    }
-  }
-`;
-const TableEx = styled(Table)`
-  margin-bottom: 15px;
-
-  .inputWrapper {
-    padding: 0;
-    position: relative;
-    text-align: left;
-
-    .field {
-      box-sizing: border-box;
-      display: block;
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 100%;
-      height: 100%;
-      border: 0;
-      padding: 0 10px;
-      outline: none;
-      font-size: 14px;
-    }
-  }
-`;
 const Manager = memo(() => {
+  /** url의 경로 구조분해 */
+  const { pathname } = useLocation();
+
   return (
     <>
       <MenuContainer>
-        <MenuLinkContainer to="customer_board">고객의소리</MenuLinkContainer>
-        <MenuLinkContainer to="test2">뉴스</MenuLinkContainer>
-        <MenuLinkContainer to="test3">공지사항</MenuLinkContainer>
+        <MenuLinkContainer to={pathname === "/manager" ? "/manager" : "/manager/manager_customer_board"}>고객의소리</MenuLinkContainer>
+        <MenuLinkContainer to="manager_news">뉴스</MenuLinkContainer>
+        <MenuLinkContainer to="manager_notice">공지사항</MenuLinkContainer>
         <MenuLinkContainer to="test4">예약</MenuLinkContainer>
         <MenuLinkContainer to="test5">회원정보</MenuLinkContainer>
         <MenuLinkContainer to="test6">회원결과</MenuLinkContainer>
         <MenuLinkContainer to="test7">병원</MenuLinkContainer>
         <MenuLinkContainer to="test8">의사</MenuLinkContainer>
-        <MenuLinkContainer to="test9">협력병원</MenuLinkContainer>
-        <MenuLinkContainer to="test10">협력의사</MenuLinkContainer>
+        <MenuLinkContainer to="manager_cooperation_hospital">협력병원</MenuLinkContainer>
+        <MenuLinkContainer to="manager_cooperation_doctor">협력의사</MenuLinkContainer>
         <MenuLinkContainer to="test11">진료과</MenuLinkContainer>
       </MenuContainer>
 
       <Routes>
-        <Route path='/customer_board' element={<ManagerCustomerBoard />} />
+        <Route path="/" element={<ManagerCustomerBoard />} />
+        <Route path="/manager_customer_board" element={<ManagerCustomerBoard />} />
+        <Route path="/manager_news" element={<ManagerNews />} />
+        <Route path="/manager_notice" element={<ManagerNotice />} />
+
+        
+        <Route path="/manager_cooperation_hospital" element={<ManagerCooperationHospital />} />
+        <Route path="/manager_cooperation_doctor" element={<ManagerCooperationDoctor />} />
       </Routes>
     </>
   );
