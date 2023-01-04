fba17f40165565a565d9c0fe4ef29e23134d5dd7
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-05 00:23:37 +0900

    feat: 추가 관리자 서브페이지

---
+++ src/pages/Manager/ManagerCooperationHospital.js
@@ -0,0 +1,90 @@
+import React, { memo } from "react";
+import styled from "styled-components";
+
+const Table = styled.table`
+  border-collapse: collapse;
+  border-top: 3px solid #168;
+  font-size: 14px;
+  text-align: center;
+  margin: auto;
+  width: 100%;
+
+  th {
+    color: #168;
+    background: #f0f6f9;
+    border: 1px solid #ddd;
+
+    &:first-child {
+      border-left: 0;
+    }
+
+    &:last-child {
+      border-right: 0;
+    }
+  }
+
+  td {
+    padding: 10px;
+    border: 1px solid #ddd;
+
+    &:first-child {
+      border-left: 0;
+    }
+
+    &:last-child {
+      border-right: 0;
+    }
+  }
+`;
+const TableEx = styled(Table)`
+  margin-bottom: 15px;
+
+  .inputWrapper {
+    padding: 0;
+    position: relative;
+    text-align: left;
+
+    .field {
+      box-sizing: border-box;
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      border: 0;
+      padding: 0 10px;
+      outline: none;
+      font-size: 14px;
+    }
+  }
+`;
+
+const ManagerCooperationHospital = memo(() => {
+  return <>
+        <Table>
+        <thead>
+          <tr>
+            <th>test</th>
+            <th>test</th>
+            <th>test</th>
+            <th>test</th>
+            <th>test</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            <tr>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+            </tr>
+          }
+        </tbody>
+      </Table>
+  </>;
+});
+
+export default ManagerCooperationHospital;
