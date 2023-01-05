f0bbd342a928e6dc345a033935de68abea5abc53
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-05 18:14:31 +0900

    comment: 추가 설명

--- src/pages/Manager/ManagerLogin.js
+++ src/pages/Manager/ManagerLogin.js
@@ -1,3 +1,10 @@
+/**
+ * @ File Name: ManagerLogin.js
+ * @ Author: 박다윗 (davidpark.0098@gmail.com)
+ * @ Last Update: 2023-01-05 17:33:33
+ * @ Description: 관리자 로그인 페이지
+ */
+
 import React, { memo } from "react";
 
 const ManagerLogin = memo(() => {
