89bee80e96a29c769ac031d71a214b38a7e834e6
Joohyeji <rosyjoo1999@gmail.com>
2023-01-10 15:58:00 +0900

    Notice 뒤로가기 오류수정

--- src/pages/NewsHome/NoticeInfo.js
+++ src/pages/NewsHome/NoticeInfo.js
@@ -5,7 +5,7 @@
  * @ Description: 공지사항상세
  */
 
-import React, { memo, useEffect, useMemo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCurrentData, getItem } from '../../slices/NoticeSlice';
@@ -96,25 +96,28 @@ const NoticeInfo = memo(() => {
 
   /** 페이지가 열린 직후 (혹은 id값이 변경된 경우) 데이터 가져오기 */
   useEffect(() => {
-    // dispatch(getItem({ id: id }));
-
-    // 이전글,다음글 가져오기위함
-    dispatch(getCurrentData());
-    //페이지 렌더 후 화면을 맨 위로 올리기
-    window.scrollTo(0, 0);
+    dispatch(getItem({ id: id }));
   }, [id]);
 
-  /** 데이터 값 변경에 따른 사이드 이펙트 처리 */
-  const item = useMemo(() => {
-    if (data) {
-      return data.find((v, i) => v.data.id == id);
-      // console.log('getCurrentData',data);
-      // return data[0];
-    } else {
-      //새로고침시 현재 데이터만 다시 로드
-      dispatch(getItem({ id: id }));
-    }
-  }, [data]);
+  // /** 데이터 값 변경에 따른 사이드 이펙트 처리 */
+  // const item = useMemo(() => {
+  //   if(data){
+  //     console.log('if 데이터',data);
+  //     // return data.data.find((v, i) => v.id == id);
+  //     // return data.data.find((v, i) => v.id == id);
+
+  //     return data[0].data;
+  //   }else{
+  //      //새로고침시 현재 데이터만 다시 로드
+  //      dispatch(getItem({ id: id }));
+  //      console.log('getItemt실행됨');
+  //   }
+  // },[data]);
+
+  if (data) {
+    console.log('지금데이터', data);
+  }
+  // if(item){console.log('지금아이템 ',item);}
 
   return (
     <Div>
@@ -127,37 +130,37 @@ const NoticeInfo = memo(() => {
       {error ? (
         <h1>에러발생함</h1>
       ) : (
-        item && (
+        data[0] && (
           <div className="pageCont">
             <Spinner loading={loading} />
             <div className="subjectArea">
               <h3 className="subject">
-                {item.data.noticeTitle}
+                {data[0].data.noticeTitle}
                 {/* 뉴 아이콘 */}
                 {dayjs(new Date()).subtract(2, 'day').format('YYYY-MM-DD') <
-                item.data.regDate ? (
+                data[0].data.regDate ? (
                   <i className="icoNew" />
                 ) : null}
               </h3>
               <div className="articleInfo">
                 <span>관리자</span>
-                <span className="itemInfo">{item.data.regDate}</span>
+                <span className="itemInfo">{data[0].data.regDate}</span>
                 <span className="itemInfo">
                   <strong>조회수 &nbsp;</strong>
                 </span>
-                <span>{item.data.hits}</span>
+                <span>{data[0].data.hits}</span>
               </div>
             </div>
             <div className="articleBody">
-              {/* <p
+              <p
                 dangerouslySetInnerHTML={{
                   __html: Buffer.from(
-                    item.data.noticeContent.data,
+                    data[0].data.noticeContent.data,
                     'base64'
                   ).toString('utf8'),
                 }}
-              ></p> */}
-              <p style={{ textAlign: 'center' }}>
+              ></p>
+              {/* <p style={{ textAlign: 'center' }}>
                 세브란스병원, NCSI 전체 산업부문 1위 달성
                 <strong>
                   존중캠페인과 비대면 의료서비스 고도화 실현해 만족도 향상 환자
@@ -173,7 +176,7 @@ const NoticeInfo = memo(() => {
               <p>‘커튼 푯말’을 설치해 환자들이 검사나 치료 중 발생할 수 있는 불필요한 신체 노출을 방지하고, 병실생활에서 지켜야 할 기본적인 에티켓을 애니메이션 교육자료로 만들어 제공하고 있다. 입원환자들을 대상으로 공감 문구를 담은 캘리그라피와 일러스트 이미지를 제작해 카카오톡으로 전송하는 ‘공감카드’에는 계절별로 힐링과 응원, 사랑, 용기 등 7가지 테마를 담았다. </p>
               <p><br/></p>
               <p>회진 게시판은 환자가 의료진에게 궁금한 점을 미리 메모해 회진시간에 자세한 설명을 들을 수 있다. 불필요한 소음과 조명을 최소화하는 수면 프로젝트와 금식 시간을 최소화하는 꿀잠과 공복탈출 프로그램으로 치유 효과와 만족도를 높였다.</p>
-              <p><br/></p>
+              <p><br/></p> */}
             </div>
 
             <div className="buttonCont">
