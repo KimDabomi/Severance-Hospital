d70bb27207e67e3d24d2c6ff2b7b50e0f55b9844
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-11 21:58:16 +0900

    feat: 수정 뉴스 페이지 redux 데이터 중복 처리를 방지하기 위하여 상태값으로 데이터 핸들링 기능 추가 검색 및 유효성 검사 메커니즘 수정 regDate의 데이터 가공처리

--- src/pages/NewsHome/NewsView.js
+++ src/pages/NewsHome/NewsView.js
@@ -4,30 +4,31 @@
  * @ Last Update: 2023-01-09 14:19:00
  * @ Description: 뉴스 하위페이지 언론보도 페이지
  */
-import React, { memo, useState, useEffect, useRef, useCallback } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { getList } from '../../slices/NewsSlice';
-import RegexHelper from '../../helper/RegexHelper';
-import { useQueryString } from '../../hooks/useQueryString';
-// 최신글 처리를 위한 dayjs
-import dayjs from 'dayjs';
 
-import Spinner from '../../components/Spinner';
+import React, { memo, useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getList } from "../../slices/NewsSlice";
+import RegexHelper from "../../helper/RegexHelper";
+import { useQueryString } from "../../hooks/useQueryString";
+// 최신글 처리를 위한 dayjs
+import dayjs from "dayjs";
+import Spinner from "../../components/Spinner";
 
 const NewsView = memo(() => {
   /** QueryString 변수 받기 */
-  const { query } = useQueryString();
+  const { query = "" } = useQueryString();
 
-  //페이지 번호
-  const page = useRef(1);
-
-  /** 화면 갱신을 위한 dummy 상태값 */
-  const [isUpdate, setUpdate] = useState(0);
+  // 페이지 번호 상태값
+  const [page, setPage] = useState(1);
+  // 검색어 상태값
+  const [queryStatus, setQueryStatus] = useState("");
+  // 데이터 관리 상태값
+  const [isData, setIsData] = useState([]);
 
   /** 리덕스 관련 초기화 */
   const dispatch = useDispatch();
-  const { pagenation, data, loading, error } = useSelector((state) => state.NewsSlice);
+  const { pagenation, loading, error } = useSelector((state) => state.NewsSlice);
 
   /** 최초 마운트시 리덕스를 통해 목록을 조회한다. */
   // 화면 새로고침에 대한 상태값이 변경된다면 데이터를 새로 로드함
@@ -35,63 +36,52 @@ const NewsView = memo(() => {
     dispatch(
       getList({
         query: query,
-        page: 1,
-        rows: 12,
+        page: page,
+        rows: 12
       })
-    );
-  }, [isUpdate, query, page]);
+    ).then((data) => {
+      if (query !== queryStatus) {
+        setIsData(data.payload.data);
+        setQueryStatus(query);
+      } else {
+        setIsData((nowData) => nowData.concat(data.payload.data));
+      }
+    });
+  }, [page, query]);
 
   /** 페이지 강제 이동을 처리하기 위한 navigate함수 생성 */
   const navigate = useNavigate();
 
-  // if (data) {
-  //   console.log('뉴스data', data, pagenation);
-  // }
-
   /** 검색했을 때 이벤트 */
-  const onSearchSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
+  const onSearchSubmit = useCallback((e) => {
+    e.preventDefault();
 
-      //입력값에 대한 유효성 검사
-      const regex = RegexHelper.getInstance();
+    const query = e.currentTarget.itemName.value;
 
-      try {
-        regex.value(
-          document.querySelector('#itemName'),
-          '검색어를 입력해주세요.'
-        );
-      } catch (e) {
-        // e.selector.focus();
-        document.querySelector('.popUpCont').style.display = 'block';
-        document.querySelector('.alert').innerHTML = e.message;
-        return;
-      }
+    //입력값에 대한 유효성 검사
+    const regex = RegexHelper.getInstance();
 
-      //검색어
-      const query = e.currentTarget.itemName.value;
-      //검색어에 따라 URL 구성
-      let redirectUrl = query ? `?query=${query}` : '/news/media.do';
-      navigate(redirectUrl);
-    },
-    [navigate]
-  );
+    try {
+      query === "" && queryStatus === "" && regex.value(query, "검색어를 입력해주세요.");
+    } catch (e) {
+      // e.selector.focus();
+      document.querySelector(".popUpCont").style.display = "block";
+      document.querySelector(".alert").innerHTML = e.message;
+      return;
+    }
+
+    //검색어에 따라 URL 구성
+    let redirectUrl = query ? `?query=${query}` : "/news/media.do";
+    navigate(redirectUrl);
+    query !== queryStatus && setPage(1);
+  });
 
   /** 더보기 버튼 (페이지) 함수 */
   const pagePlus = useCallback((e) => {
-    console.log('더보기버튼 누름', page);
-    //페이지 번호 1증가
-    page.current++;
-
-    //추가 검색 결과를 요청
-    dispatch(
-      getList({
-        query: query,
-        page: page.current,
-      })
-    );
+    console.log("더보기버튼 누름", page);
+    setPage(page + 1);
   });
-  
+
   /** 닫기버튼 눌렸을 때 */
   const closeClick = useCallback((e) => {
     document.querySelector(".popUpCont").style.display = "none";
@@ -109,9 +99,11 @@ const NewsView = memo(() => {
                 <input
                   type="text"
                   name="itemName"
-                  id='itemName'
+                  id="itemName"
                   placeholder="검색어를 입력해 주세요"
                   className="formControl"
+                  defaultValue={query}
+                  key={query}
                 />
                 <span>
                   <button type="submit" className="btnSearch">
@@ -124,32 +116,24 @@ const NewsView = memo(() => {
 
           {error ? (
             <h1>에러발생함</h1>
-          ) : data && data[0] ? (
+          ) : isData && pagenation ? (
             <div>
               <Spinner loading={loading} />
               {/* 검색결과 */}
               <div className="bbsList">
-                {data.map((v, i) => {
+                {isData.map((v, i) => {
+                  const regDate = v.regDate.slice(0,10);
                   return (
                     <div className="bbsItem" key={v.id}>
-                      <a
-                        className="inner"
-                        href={v.newsLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
+                      <a className="inner" href={v.newsLink} rel="noopener noreferrer" target="_blank">
                         <div className="bbssubjectArea">
                           <strong>{v.newsTitle}</strong>
 
                           {/* 뉴 아이콘 */}
-                          {dayjs(new Date())
-                            .subtract(2, 'day')
-                            .format('YYYY-MM-DD') < v.regDate ? (
-                            <i className="icoNew" />
-                          ) : null}
+                          {dayjs(new Date()).subtract(2, "day").format("YYYY-MM-DD") < regDate ? <i className="icoNew" /> : null}
                         </div>
                         <div className="infoArea">
-                          <span className="date">{v.regDate}</span>
+                          <span className="date">{regDate}</span>
                         </div>
                       </a>
                     </div>
@@ -160,9 +144,9 @@ const NewsView = memo(() => {
               {/* 페이지가 2페이지 이상일 경우 더보기 버튼 */}
               {pagenation.nowPage !== pagenation.totalPage ? (
                 <div className="buttonContColumn">
-                  <Link className="btnMore" onClick={pagePlus}>
+                  <button type="button" className="btnMore" onClick={pagePlus}>
                     더보기
-                  </Link>
+                  </button>
                 </div>
               ) : null}
             </div>
