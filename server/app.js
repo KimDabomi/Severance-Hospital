ecd686658c0e323c5d935503d482468fe7b36a96
otw03 <86701873+otw03@users.noreply.github.com>
2023-01-12 09:34:24 +0900

    병원, 의사 테이블 추가

--- server/app.js
+++ server/app.js
@@ -148,6 +148,14 @@ app.use(require('./controllers/NoticeController'));
 // 회원정보
 app.use(require('./controllers/UserInfoController'));
 
+// 의사
+app.use(require('./controllers/DoctorController'));
+
+// 병원
+app.use(require('./controllers/HospitalController'));
+
+// 진료과
+app.use(require('./controllers/DepartmentController'));
 
 app.use((err, req, res, next) => res.sendError(err));
 app.use("*", (req, res, next) => res.sendError(new PageNotFoundException()));
