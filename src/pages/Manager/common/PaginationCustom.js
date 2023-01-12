07b415fc86bcac157c68e09fc892f5b65340fe2c
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-12 12:44:39 +0900

    feat: 추가 pagination 색상 커스텀

--- src/pages/Manager/common/PaginationCustom.js
+++ src/pages/Manager/common/PaginationCustom.js
@@ -15,11 +15,14 @@ import PaginationItem from "@mui/material/PaginationItem";
 
 /**
  * @page useQueryString()을 통해 가져온 값
- * @pagenation Redux의 pagenation값
+ * @pagenation Redux의 pagenation
+ * @totalPage 전체 페이지 수
  * @pageQueryPath 현재 페이지 번호를 QueryString으로 표현하기 위한 Path
  * @query 검색어를 QueryString으로 가져온 값
+ * @color pagination의 커스텀 폰트 컬러
+ * @bgColor pagination의 커스텀 배경 컬러
  */
-const PaginationCustom = memo(({ page, pagenation, pageQueryPath, query }) => {
+const PaginationCustom = memo(({ page, pagenation: { totalPage }, pageQueryPath, query, color = "rgba(0,0,0,0.87)", bgColor = "rgba(0,0,0,0.0.8)" }) => {
   /** 페이지 구성 */
   const nowPage = parseInt(page || "1", 10);
 
@@ -30,8 +33,8 @@ const PaginationCustom = memo(({ page, pagenation, pageQueryPath, query }) => {
   const paginationStyle = makeStyles((theme) => ({
     root: {
       "& .css-yuzg60-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected": {
-        backgroundColor: "#168",
-        color: "#fff"
+        backgroundColor: bgColor,
+        color: color
       }
     }
   }));
@@ -47,7 +50,7 @@ const PaginationCustom = memo(({ page, pagenation, pageQueryPath, query }) => {
 
   return (
     <Pagination
-      count={pagenation.totalPage}
+      count={totalPage}
       defaultPage={1}
       siblingCount={2}
       boundaryCount={1}
