c3695bebdd549e5faa8f1a45d7ee9d4e2e5b60ac
Kimdabomi <cdabomi60@gmail.com>
2023-01-12 17:40:20 +0900

    회원가입 페이지 수정, 페이지네이션 수정

--- server/app.js
+++ server/app.js
@@ -149,6 +149,50 @@ app.use(require('./controllers/NoticeController'));
 app.use(require('./controllers/UserInfoController'));
 
 
+// 로그인
+// router.post('/login',(req,res,next) => {
+//     // POST 로 전송된 변수값을 추출
+//     const userId = req.body.userId;
+//     const userPassword = req.body.userPassword;
+    
+//     // 세션 저장
+//     req.session.userId = userId;
+//     req.session.userPassword = userPassword;
+
+//     // 결과 응답
+//     const json = {rt:'ok'};
+//     res.status(200).send(json);
+// })
+// .get('/login',(req,res,next) => {
+//     // 저장되어 있는 모든 session 값 탐색
+//     for (let key in req.session) {
+//         const str = '[session] ' + key + '=' + req.session[key];
+//         logger.debug(str); 
+//     }
+
+//     // 세션 데이터를 json으로 구성 후 클라이언트에게 응답으로 전송
+//     const my_data = {
+//       userId: req.session.userId,
+//       userPassword: req.session.userPassword
+//     };
+
+//     res.status(200).send(my_data);
+// })
+// .delete('/login',async(req,res,next) => {
+//     let result = 'ok';
+//     let code = 200;
+
+//     try {
+//         await req.session.destroy();
+//     } catch (e) {
+//         logger.error(e.message);
+//         result = e.message;
+//         code = 500;
+//     }
+//     const json = {rt:result};
+//     res.status(code).send(json);
+// });
+
 app.use((err, req, res, next) => res.sendError(err));
 app.use("*", (req, res, next) => res.sendError(new PageNotFoundException()));
 
