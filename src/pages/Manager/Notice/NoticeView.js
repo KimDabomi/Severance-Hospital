99fdb7c25fb3a7a5ff0d562127510eed9505ff9e
davidpark-0098 <davidpark.0098@gmail.com>
2023-01-14 23:40:14 +0900

    feat: 추가 관리자 공지사항

---
+++ src/pages/Manager/Notice/NoticeView.js
@@ -0,0 +1,14 @@
+/**
+ * @ File Name: NoticeView.js
+ * @ Author: 박다윗 (davidpark.0098@gmail.com)
+ * @ Last Update: 2023-01-13 00:33:33
+ * @ Description: 관리자 공지사항 보기
+ */
+
+import React, { memo } from "react";
+
+const NoticeView = memo(() => {
+  return <div>view</div>;
+});
+
+export default NoticeView;
